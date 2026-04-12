7f5ace0d55240d2193f08b05481b0b1a9e886c37
winyunq <winyunq@qq.com>
2026-04-12 13:25:09 +0800

    fix error

--- Source/UmgMcp/Public/Animation/UmgMcpSequencerCommands.h
+++ Source/UmgMcp/Public/Animation/UmgMcpSequencerCommands.h
@@ -7,6 +7,8 @@
 #include "MovieScene.h"
 #include "MovieSceneTrack.h"
 
+class UWidgetBlueprint;
+
 /**
  * @brief Handles all MCP commands for querying and manipulating UMG Animations (Sequencer).
  * Updated for v2.0 Read-Write-Attention API.
@@ -20,9 +22,18 @@ public:
     TSharedPtr<FJsonObject> HandleCommand(const FString& Command, const TSharedPtr<FJsonObject>& Params);
 
 private:
+    TSharedPtr<FJsonObject> ResolveAnimationContext(
+        const TSharedPtr<FJsonObject>& Params,
+        UWidgetBlueprint*& OutBlueprint,
+        UWidgetAnimation*& OutAnimation,
+        FString& OutError) const;
+    bool ResolveWidgetName(const TSharedPtr<FJsonObject>& Params, FString& OutWidgetName) const;
+
     // Attention (Context)
     TSharedPtr<FJsonObject> SetAnimationScope(const TSharedPtr<FJsonObject>& Params);
+    TSharedPtr<FJsonObject> SetAnimationTarget(const TSharedPtr<FJsonObject>& Params);
     TSharedPtr<FJsonObject> SetWidgetScope(const TSharedPtr<FJsonObject>& Params);
+    TSharedPtr<FJsonObject> SetWidgetTarget(const TSharedPtr<FJsonObject>& Params);
 
     // Read (Sensing)
     TSharedPtr<FJsonObject> GetAllAnimations(const TSharedPtr<FJsonObject>& Params);
@@ -30,6 +41,9 @@ private:
     TSharedPtr<FJsonObject> GetAnimatedWidgets(const TSharedPtr<FJsonObject>& Params);
     TSharedPtr<FJsonObject> GetAnimationFullData(const TSharedPtr<FJsonObject>& Params);
     TSharedPtr<FJsonObject> GetWidgetAnimationData(const TSharedPtr<FJsonObject>& Params);
+    TSharedPtr<FJsonObject> GetWidgetPropertyTimeline(const TSharedPtr<FJsonObject>& Params);
+    TSharedPtr<FJsonObject> GetTimeSliceProperties(const TSharedPtr<FJsonObject>& Params);
+    TSharedPtr<FJsonObject> GetAnimationOverview(const TSharedPtr<FJsonObject>& Params);
 
     // Write (Action)
     TSharedPtr<FJsonObject> CreateAnimation(const TSharedPtr<FJsonObject>& Params);
@@ -37,6 +51,9 @@ private:
     TSharedPtr<FJsonObject> SetPropertyKeys(const TSharedPtr<FJsonObject>& Params);
     TSharedPtr<FJsonObject> RemovePropertyTrack(const TSharedPtr<FJsonObject>& Params);
     TSharedPtr<FJsonObject> RemoveKeys(const TSharedPtr<FJsonObject>& Params);
+    TSharedPtr<FJsonObject> AppendWidgetTracks(const TSharedPtr<FJsonObject>& Params);
+    TSharedPtr<FJsonObject> AppendTimeSlice(const TSharedPtr<FJsonObject>& Params);
+    TSharedPtr<FJsonObject> DeleteWidgetKeys(const TSharedPtr<FJsonObject>& Params);
     
     // Level 2 API
     TSharedPtr<FJsonObject> SetAnimationData(const TSharedPtr<FJsonObject>& Params);
